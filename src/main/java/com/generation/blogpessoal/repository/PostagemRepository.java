5c4b53d9d1dbf55a75981d84759c2c178d8a1ef1
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-10-14 11:46:27 -0300

    :sparkles: Finalização do CRUD do Recurso Postagem

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	// SELECT * FROM tb_postagens WHERE titulo LIKE "%?%";
 
 }
